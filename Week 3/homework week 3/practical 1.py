08fb9766917ed5bdcc441beba4abe1c1593b6e91
zoelet03 <zoeletobrenyah@gmail.com>
2023-10-09 10:18:39 +0100

    Week 3 files updated

---
+++ Week 3/homework week 3/practical 1.py
@@ -0,0 +1,7 @@
+name = input('Hello, what is your name? ')
+if name == "":
+    print('Hello Stranger!')
+
+
+print(f'Hello, {name}. It is good to meet you.')
+
--- Week 3/week3.py
+++ Week 3/week3.py
@@ -1,14 +1,46 @@
+"""
 #StudentGrades
-
-maths = 7
-english = 7
-biology = 9
-chemistry = 6
-geography = 6
+maths = 70
+english = 75
+biology = 90
+chemistry = 60
+geography = 60
 
 avg = (maths + english + biology + chemistry + geography)/5
 
 print("Your average grade achieved is", avg)
+"""
+"""
+#WeatherStation
+
+
+weather = [10, 14, 18, 20, 16]
+
+print("Today's weather will be an average of:", (sum(weather)/5))
+print("With highs of:", (max(weather)))
+print("And lows of:", (min(weather)))
+
+
+
+grades = [65, 70, 45, 85, 90]
+
+print("Your lowest grade was:", (min(grades)))
+print("Your highest grade was:", (max(grades)))
+
+print("Your grade average is:", (sum(grades)/5))
+""""""
+#Excercise03
+
+
+
+
+
+
+
+
+
+
+
 
 
 
