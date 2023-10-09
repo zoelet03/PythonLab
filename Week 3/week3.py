a08f6fcf301779c568e97b16fca4d334be066b05
zoelet03 <145971725+zoelet03@users.noreply.github.com>
2023-10-09 10:03:12 +0100

    Week 3 files updated

---
+++ Week 3/week3.py
@@ -0,0 +1,14 @@
+#StudentGrades
+
+maths = 7
+english = 7
+biology = 9
+chemistry = 6
+geography = 6
+
+avg = (maths + english + biology + chemistry + geography)/5
+
+print("Your average grade achieved is", avg)
+
+
+
